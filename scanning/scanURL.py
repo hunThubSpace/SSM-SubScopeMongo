c3234e7b87ce2c5698e986778ef6775c04170cb4
hunThub <49031710+hunThubSpace@users.noreply.github.com>
2024-11-18 21:53:52 +0330

    Delete scanning directory

--- scanning/scanURL.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
-
-def run_nuclei(urls):
-    # Placeholder for actual nuclei logic
-    print("Running nuclei on urls:")
-    for url in urls:
-        print(url['url'])
